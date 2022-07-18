8273bbdd3620e66ee066399f075cdc556e41f893
Superfly <danielukoha101@gmail.com>
2022-07-18 14:54:14 +0100

    adding data fetching logic

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,35 +1,61 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Superfly",
-    text: "She doesn't want you, she just want attention.",
-  },
-  {
-    id: "q2",
-    author: "Superfly",
-    text: "From the street she did emerge, and to the streets she shall return.",
-  },
-];
+// const DUMMY_QUOTES = [
+//   {
+//     id: "q1",
+//     author: "Superfly",
+//     text: "She doesn't want you, she just want attention.",
+//   },
+//   {
+//     id: "q2",
+//     author: "Superfly",
+//     text: "From the street she did emerge, and to the streets she shall return.",
+//   },
+// ];
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
       <h1>Quote detail</h1>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
 
       <Route path={match.path} exact>
         <div className="centered">
